14a973a945c892a7b834ef4239720984533fd153
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2018-06-07 15:26:25 +0530

    Initial setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Homepage from './components/Homepage';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Homepage />
+    );
+  }
+}
