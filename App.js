9ddee62206ab0dfc4e0a09accb643474af726bf5
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2018-06-10 11:20:32 +0530

    Revert "Node version fixed to 8, Folder structure and redux added to project"
    This reverts commit 70d9b5230595a454a081a8c7ac0bfbb25e3e02d5.

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Provider } from 'react-redux';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { configureStore } from './app/reduxStore';
-import Homepage from './app/components/Homepage';
-
-const store = configureStore();
+import Homepage from './components/Homepage';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <Homepage />
-      </Provider>
+      <Homepage />
     );
   }
 }
--- components/Homepage.js
+++ components/Homepage.js
@@ -4,16 +4,9 @@ import { Text, View } from 'react-native';
 import styles from '../styles/style';
 
 export default class Homepage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      test: true
-    };
-  }
-
   render() {
     return (
-      <View style={styles.appContainer}>
+      <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
